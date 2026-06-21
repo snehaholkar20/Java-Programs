a16af006b67a68225bd168975a11bdabaa691ba5
Sneha Ram Holkar <snehaholkar20@gmail.com>
2026-06-21 22:27:59 +0530

    Add Array1Dprg1.java to print array elements

---
+++ Arrays/Array1Dprg1.java
@@ -0,0 +1,12 @@
+class Array1Dprg1
+ {
+  public static void main(String args[])
+    {
+      int[]num={10,20,30,40,50};
+       int i;
+        for(i=0;i<5;i++)
+       {
+         System.out.println(num[i]);
+       }
+    }
+ }
